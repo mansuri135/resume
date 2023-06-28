2ac5d7ea5bf18044658ce984b1148484675910b3
minhaj9298 <120560670+minhaj9298@users.noreply.github.com>
2023-06-28 18:23:20 +0530

    add active class on tab js

--- components/Portfolio/Tabs.js
+++ components/Portfolio/Tabs.js
@@ -1,23 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
 function Tabs({ filterCategory, tabsData }) {
+  const [activeButtonIndex, setActiveButtonIndex] = useState(0);
 
-    
+  const handleButtonClick = (category, index) => {
+    setActiveButtonIndex(index);
+    filterCategory(category);
+  };
 
-    return (
-        <>
+  useEffect(() => {
+    // Add "active" class to the first button when the component mounts
+    const buttons = document.getElementsByClassName("filterButton");
+    if (buttons.length > 0) {
+      buttons[0].classList.add("active");
+    }
+  }, []);
 
-            <div className="text-center my-4">
-                {
-                    tabsData.map((category, index) => {
-                        return (
-                            <button type="button" className="filterButton mx-2 text-capitalize" onClick={() => filterCategory(category)} key={index}>{category}</button>
-                        )
-                    })
-                }
-
-
-            </div>
-        </>
-    )
+  return (
+    <div className="text-center my-4">
+      {tabsData.map((category, index) => (
+        <button
+          type="button"
+          className={`filterButton mx-2 text-capitalize${activeButtonIndex === index ? ' active' : ''}`}
+          onClick={() => handleButtonClick(category, index)}
+          key={index}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Tabs;
